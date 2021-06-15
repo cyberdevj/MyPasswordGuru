a6c1c5255ee363838735e195826f3a307162e277
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-06-15 23:43:47 +0800

    Refactored logout.js

--- src/component/Logout.js
+++ src/component/Logout.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import faker from "faker";
+import UISegmentWithProfilePicture from "./UISegmentWithProfilePicture";
 
 const Logout = () => {
     return (
-        <div className="ui segment">
-            <div className="ui items">
-                <div className="item">
-                    <div className="ui tiny image">
-                        <img alt="ProfilePicture" src={faker.image.avatar()} />
-                    </div>
-                    <div className="content">
-                        <div className="header">{faker.name.firstName()}</div>
-                        <div className="description">
-                            <Link className="ui fluid button" to="/">Logout</Link>
-                        </div>
-                    </div>
-                </div>
+        <UISegmentWithProfilePicture src={faker.image.image()}>
+            <div className="header">{faker.company.companyName()}</div>
+            <div className="description">
+                <Link className="ui fluid button" to="/">Logout</Link>
             </div>
-        </div>
+        </UISegmentWithProfilePicture>
     );
 };
 
