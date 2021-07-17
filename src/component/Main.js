3b9b6fb65aea15ce75baa9faca6ee94e5a453958
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-07-17 19:58:14 +0800

    Integrate authentication services with Main.js

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AuthenticationService from "./AuthenticationService";
 import { Link } from "react-router-dom";
 
-class Main extends React.Component {
-    render() {
-        return (
-            <div className="ui center-screen">
-                <h1 className="ui header">
-                    <div className="content">Password Guru</div>
-                </h1>
-                <div className="ui form">
-                    <div className="field w-250">
-                        <label>PIN</label>
-                        <input type="text" name="pin" placeholder="pin" />
+const Main = () => {
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        setUsername(AuthenticationService.getUser());
+    });
+
+    return (
+        <div className="ui center-screen">
+            <h1 className="ui header">
+                <div className="content">Password Guru</div>
+            </h1>
+            <div className="ui green message" hidden={!username}>Logged in as {username}</div>
+            <div className="ui form">
+                <div className="field w-250">
+                    <label>PIN</label>
+                    <input type="password" name="pin" placeholder="pin" />
+                </div>
+                <div className="ui grid">
+                    <div className="eight wide column">
+                        <button className="ui fluid blue button" to="/login/list" disabled={!username}>Login</button>
                     </div>
-                    <Link className="ui primary submit button" to="/login/list">Login</Link>
-                    <Link className="ui button" to="/login/list">New</Link>
+                    <div className="eight wide column">
+                        <Link className="ui fluid green button" to="/account/new">New</Link>
+                    </div>
                 </div>
             </div>
-        );
-    };
+        </div>
+    );
 };
 
 export default Main
