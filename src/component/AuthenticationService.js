09d398b3129c60026953428c6367dd22fc2bba10
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-07-19 04:29:19 +0800

    Refactored and updated AuthenticationService with new function handlers for data manipulation, changed data separator.

--- src/component/AuthenticationService.js
+++ src/component/AuthenticationService.js
@@ -5,6 +5,7 @@ import HmacSHA256 from "crypto-js/hmac-sha256";
 import SHA256 from "crypto-js/sha256";
 
 let state = {
+    separator: "@@@",
     aes: {
         options: {
             mode: CryptoJS.mode.CBC,
@@ -19,31 +20,12 @@ let state = {
     }
 };
 
-let verifySHA256 = (message, hash) => {
-    return SHA256(message).toString() === hash;
-};
-
-let getPBKDF2 = (key) => {
-    let salt = CryptoJS.lib.WordArray.random(128 / 8);
-    return {
-        key: PBKDF2(key, salt, state.pbkdf2.options).toString(),
-        salt: salt
-    };
-};
-
-let getAesAndHmacEncryption = (data, key) => {
-    let pbkdf = getPBKDF2(key);
-    
-    let aesData = AES.encrypt(data, pbkdf["key"], state.aes.options).toString() + "|" + pbkdf["salt"];
-    return aesData + "|" + HmacSHA256(aesData, pbkdf["key"]);
-};
-
-let getAuthData = () => {
+let getAuthObj = () => {
     let data = localStorage.getItem("authentication_data");
     if (!data)
         return null;
 
-    data = data.split("@@@");
+    data = data.split(state.separator);
     if (data.length !== 2)
         return null;
     if (!verifySHA256(data[0], data[1]))
@@ -57,6 +39,67 @@ let getAuthData = () => {
     return data;
 };
 
+let getAuthItem = () => {
+    let data = getAuthObj();
+    let encryptedData = data["data"].split(state.separator)[0];
+    let decryptedData = decryptAES(encryptedData, localStorage.getItem("sessionKey"));
+    return JSON.parse(decryptedData);
+};
+
+let getAuthSalt = () => {
+    let data = getAuthObj();
+    return data["data"].split(state.separator)[1];
+}
+
+let verifySHA256 = (message, hash) => {
+    return SHA256(message).toString() === hash;
+};
+
+let verifyHmac = (message, key, hash) => {
+    return HmacSHA256(message, key).toString() === hash;
+}
+
+let validateSessionKey = (data, key) => {
+    data = data.split(state.separator);
+    if (data.length !== 3)
+        return false;
+
+    if (key)
+        key = PBKDF2(key, data[1], state.pbkdf2.options).toString();
+    else
+        key = localStorage.getItem("sessionKey");
+
+    if (!verifyHmac(data[0] + state.separator + data[1], key, data[2]))
+        return false;
+
+    localStorage.setItem("sessionKey", key);
+    return true;
+}
+
+let getPBKDF2 = (key) => {
+    let salt = CryptoJS.lib.WordArray.random(128 / 8).toString();
+    return {
+        key: PBKDF2(key, salt, state.pbkdf2.options).toString(),
+        salt: salt
+    };
+}
+
+let encryptAndStore = (authObj, key, salt) => {
+    authObj["data"] = encryptAES(JSON.stringify(authObj["data"]), key, salt);
+
+    let authBase64 = encryptBase64(JSON.stringify(authObj));
+    localStorage.setItem("authentication_data", authBase64 + state.separator + CryptoJS.SHA256(authBase64).toString());
+};
+
+let encryptAES = (data, key, salt) => {
+    data = AES.encrypt(data, key, state.aes.options).toString() + state.separator + salt;
+    return data + state.separator + HmacSHA256(data, key);
+};
+
+let decryptAES = (data, key) => {
+    return AES.decrypt(data, key, state.options).toString(CryptoJS.enc.Utf8);
+};
+
 let encryptBase64 = (data) => {
     data = CryptoJS.enc.Utf8.parse(data);
     return CryptoJS.enc.Base64.stringify(data);
@@ -67,23 +110,106 @@ let decryptBase64 = (data) => {
     return CryptoJS.enc.Utf8.stringify(data);
 };
 
+let OKStatus = (data) => {
+    return { status: "OK", data: data };
+}
+
+let ERRStatus = (data) => {
+    let statusResponse = { status: "ERR" };
+    if (data)
+        statusResponse["data"] = data;
+    return statusResponse;
+}
+
 const AuthenticationService = {
+    getFaviconUrl: function(url) {
+        return `https://s2.googleusercontent.com/s2/favicons?domain_url=${url}`;
+    },
+    getHttps: function(url) {
+        if (!/^https?:\/\//i.test(url))
+            url = 'http://' + url;
+        return url;
+    },
+    getItemFromList: function(id, list) {
+        for (let i in list)
+            if (list[i]["id"] === parseInt(id))
+                return list[i];
+        return null;
+    },
+    updateItemFromList: function(id, list, value) {
+        for (let i in list)
+            if (list[i]["id"] === parseInt(id))
+                list[i] = value;
+        return list;
+    },
+    getLastId: function(data) {
+        let id = 0;
+        for (let i in data)
+            if (id < data[i]["id"])
+                id = data[i]["id"];
+        return id;
+    },
     getUser: function() {
-        let data = getAuthData();
+        let data = getAuthObj();
         if (!data)
             return null;
 
         if (!("username" in data))
             return null;
-        return data["username"];
-    },
-    setAuth: function(authObj, key, callback) {
-        authObj["data"] = getAesAndHmacEncryption(JSON.stringify(authObj["data"]), key);
-        authObj["hash"] = SHA256(JSON.stringify(authObj));
 
-        let authBase64 = encryptBase64(JSON.stringify(authObj));
-        localStorage.setItem("authentication_data", authBase64 + "@@@" + CryptoJS.SHA256(authBase64).toString());
-        callback({ "status": "OK" });
+        return OKStatus(data["username"]);
+    },
+    generateEncryptedAuthObj: function(authObj, key, callback) {
+        let pbkdf = getPBKDF2(key);
+
+        encryptAndStore(authObj, pbkdf.key, pbkdf.salt);
+
+        callback(OKStatus());
+    },
+    get: function(key) {
+        if (!this.sessionValid())
+            return ERRStatus("Invalid session");
+
+        let data = getAuthItem()
+
+        return OKStatus(data[key]);
+    },
+    set: function(key, value) {
+        if (!this.sessionValid())
+            return ERRStatus("Invalid session");
+
+        let data = getAuthItem();
+        data[key] = value;
+
+        let authObj = getAuthObj();
+        authObj["data"] = data;
+        encryptAndStore(authObj, localStorage.getItem("sessionKey"), getAuthSalt());
+
+        return OKStatus(data);
+    },
+    sessionStart: function(key) {
+        let data = getAuthObj();
+        
+        if (!validateSessionKey(data["data"], key))
+            return null;
+        return OKStatus();
+    },
+    sessionValid: function() {
+        if (!localStorage.getItem("sessionKey")) {
+            localStorage.removeItem("sessionKey");
+            return false;
+        }
+        
+        let data = getAuthObj();
+        if (!validateSessionKey(data["data"])) {
+            localStorage.removeItem("sessionKey");
+            return false;
+        }
+        return true;
+            
+    },
+    sessionDestroy: function() {
+        localStorage.removeItem("sessionKey");
     }
 };
 
