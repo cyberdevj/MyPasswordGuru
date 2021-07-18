2f37b0e8c61c691c2a5ef1d289aa7019e20dd402
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-07-19 04:34:09 +0800

    Enable feature to view actual login data

--- src/component/LoginAccountView.js
+++ src/component/LoginAccountView.js
@@ -1,43 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import faker from "faker";
 import UISegmentWithProfilePicture from "./UISegmentWithProfilePicture";
 import UISegmentWithHeader from "./UISegmentWithHeader";
 import UITextField from "./UITextField";
+import AuthenticationService from "./AuthenticationService";
 
-class LoginAccountView extends React.Component {
-    render() {
-        return (
-            <form className="ui form">
-                <h1 className="ui header">
-                    <div className="ui content">Account Information</div>
-                </h1>
-                
-                <UISegmentWithProfilePicture src={faker.image.image()}>
-                    <div className="header">{faker.company.companyName()}</div>
-                    <div className="description">
-                        <button className="ui fluid button">Go to Login Page</button>
-                    </div>
-                </UISegmentWithProfilePicture>
+const LoginAccountView = () => {
+    const [login, setLogin] = useState({});
 
-                <UISegmentWithHeader header="Credentials">
-                    <UITextField label="Username" type="text" name="username" defaultValue={faker.internet.userName()} isReadOnly={true} />
-                    <UITextField label="Password" type="password" name="username" defaultValue={faker.internet.userName()} iconCss="copy outline icon" isReadOnly={true} />
-                </UISegmentWithHeader>
+    const history = useHistory();
+    const params = useParams();
 
-                <UISegmentWithHeader header="One Time Password">
-                    <UITextField type="text" name="oneTimePassword" defaultValue={faker.datatype.number(999999)} isReadOnly={true} />
-                </UISegmentWithHeader>
-                
-                <UISegmentWithHeader header="URI">
-                    <UITextField type="text" name="uri1" defaultValue={faker.internet.url()} iconCss="external alternate icon" isReadOnly={true} />
-                </UISegmentWithHeader>
+    useEffect(() => {
+        if (!AuthenticationService.sessionValid())
+            history.push("/");
 
-                <Link className="ui icon button positive" to="/login/edit"><i className="edit outline icon"></i></Link>
-                <Link className="ui icon button" to="/login/list"><i className="arrow alternate circle left icon"></i></Link>
-            </form>
-        );
-    }
+        let logins = AuthenticationService.get("logins");
+        if (logins["status"] !== "OK") {
+            AuthenticationService.sessionDestroy();
+            history.push("/");
+        }
+
+        let data = AuthenticationService.getItemFromList(params["id"], logins["data"]);
+        setLogin(data);
+    }, []);
+
+    const deleteLogin = (e) => {
+        e.preventDefault();
+        let logins = AuthenticationService.get("logins");
+        logins = logins["data"].filter((obj) => {
+            return obj.id !== parseInt(params["id"]);
+        });
+
+        AuthenticationService.set("logins", logins);
+        history.push("/login/list");
+    };
+
+    return (
+        <form className="ui form">
+            <h1 className="ui header">
+                <div className="ui content">Account Information</div>
+            </h1>
+            
+            <UISegmentWithProfilePicture src={AuthenticationService.getFaviconUrl(login["url"])}>
+                <div className="header">{login["name"]}</div>
+                <div className="description">
+                    <a className="ui fluid button" href={AuthenticationService.getHttps(login["url"])} target="_black">Go to Login Page</a>
+                </div>
+            </UISegmentWithProfilePicture>
+
+            <UISegmentWithHeader header="Credentials">
+                <UITextField label="Username" type="text" name="username" defaultValue={login["username"]} isReadOnly={true} />
+                <UITextField label="Password" type="password" name="username" defaultValue={login["password"]} iconCss="copy outline icon" isReadOnly={true} />
+            </UISegmentWithHeader>
+
+            <UISegmentWithHeader header="One Time Password">
+                <UITextField type="text" name="oneTimePassword" defaultValue={login["otp"]} isReadOnly={true} />
+            </UISegmentWithHeader>
+            
+            <UISegmentWithHeader header="URI">
+                <UITextField type="text" name="uri1" defaultValue={login["url"]} iconCss="external alternate icon" isReadOnly={true} />
+            </UISegmentWithHeader>
+
+            <Link className="ui icon button positive" to={`/login/edit/${login["id"]}`}><i className="edit outline icon"></i></Link>
+            <Link className="ui icon button" to="/login/list"><i className="window close outline icon"></i></Link>
+            <div className="ui right floated">
+                <button className="ui icon button red" onClick={deleteLogin}><i className="eraser icon"></i></button>
+            </div>
+        </form>
+    );
 };
 
 export default LoginAccountView;
