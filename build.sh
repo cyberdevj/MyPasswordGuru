216c02e4c0159dbe52ffc57f74cbff488434f25c
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-07-21 16:09:36 +0800

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-buildFolder="./build/*"
+buildLocation="./build/*"
+appName="MyPasswordGuru"
 
 echo "[I] Building MyPasswordGuru ...";
 INLINE_RUNTIME_CHUNK=false npm run build
-echo "[I] Build successful";
+echo "[I] Build successful, zipping files for deployment";
+zip -r ${appName}.zip $buildLocation
