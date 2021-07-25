cb485d05bac17ea93894e44b7ceaf40fab1fd981
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-07-26 01:58:41 +0800

    Added closing functionality for react card component

--- src/component/UICardItem.js
+++ src/component/UICardItem.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const UICardItem = (props) => {
     return (
-        <div className={`ui card ${props.className}`}>
+        <div className={`ui card ${props.className}`} onClick={props.onClick}>
             <div className="content">
+                {props.close ? <div className="right floated icon" onClick={props.closeOnClick}><i className="close icon"></i></div> : null}
                 <div className="header">{props.title}</div>
                 <div className="meta">{props.meta}</div>
             </div>
