c28c13d98e6d701851214529845103b374999c10
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-07-26 01:56:42 +0800

    Added back capability for react component segment classes

--- src/component/UISegmentWithHeader.js
+++ src/component/UISegmentWithHeader.js
@@ -5,7 +5,14 @@ const UISegmentWithHeader = (props) => {
     return (
         <UISegment>
             <h2 className="ui header">
-                <div className="content">{props.header}</div>
+                <div className="content">
+                    {props.header}
+                </div>
+                {props.backButton ?
+                <div className="content right floated cursor-pointer">
+                    <i className="arrow alternate circle left outline icon" onClick={props.backOnClick}></i>
+                </div> 
+                : null}
             </h2>
             <div className="content">
                 {props.children}
