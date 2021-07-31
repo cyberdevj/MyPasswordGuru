09b5d12e74e6e1d65ca5b9179907637450c47883
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-08-01 06:06:36 +0800

    - Improved checkbox component to allow sub label

--- src/component/UICheckBox.js
+++ src/component/UICheckBox.js
@@ -4,9 +4,13 @@ const UICheckBox = (props) => {
     return (
         <div className={`${props.className}`}>
             <div className="ui segment" onClick={props.onClick}>
-                <div className={`ui checkbox ${props.checked}`}>
-                    <input type="checkbox" value={props.value} onChange={props.onChange} checked={props.checked} defaultChecked={props.defaultChecked} readOnly={props.readOnly} />
-                    <label className="text-bold cursor-default">{props.label}</label>
+                <div className={`ui checkbox`}>
+                    <input type="checkbox" name={props.name} value={props.value} onChange={props.onChange} checked={props.checked} defaultChecked={props.defaultChecked} readOnly={props.readOnly} />
+                    <label className="cursor-default">
+                        <span className="text-bold">{props.label}</span>
+                        <br />
+                        <span className="sub header">{props.sub}</span>
+                    </label>
                 </div>
             </div>
         </div>
