636d9bf77ecba143b067304318962b4888807ab8
Manuel Juan Martinez <dotjuan89@gmail.com>
2021-08-09 13:26:24 +0800

    - Fixed layout issue of ui range component (range input overlapping number input)

--- src/component/UIRange.js
+++ src/component/UIRange.js
@@ -2,16 +2,20 @@ import React from "react";
 
 const UIRange = (props) => {
     return(
-        <div className="ui segment grid">
-            <div className="four wide column">
-                <div className="ui labeled input">
-                    <div className="ui label">{props.label}</div>
-                    <input id={props.id} type="text" max={props.max} value={props.value} onChange={props.onChange} />
+        <div className="ui form">            
+            <div className="ui segment grid">
+                <div className="four wide column">
+                    <div className="field">
+                        <div className="ui labeled input">
+                            <div className="ui label">{props.label}</div>
+                            <input id={props.id} type={props.type} max={props.max} value={props.value} onChange={props.onChange} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="twelve wide column">
-                <div className="ui input pl-50 w-100">
-                    <input type="range" max={props.max} value={props.value} onChange={props.onChange} />
+                <div className="right floated ten wide column">
+                    <div className="ui input w-100">
+                        <input type="range" max={props.max} value={props.value} onChange={props.onChange} />
+                    </div>
                 </div>
             </div>
         </div>
